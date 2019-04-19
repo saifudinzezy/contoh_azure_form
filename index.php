0e08704bf33bac47d84d9f221791208145182a94
saifudin <saifudin.zezy7@gmail.com>
2019-04-19 19:49:48 +0700

    tambahan

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "registrasi.database.windows.net";
+    $user = "saifudin";
+    $pass = "Dirimu_1";
+    $db = "registrasi";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
